9a279ce2937f84efc6aa5f37f4c7f60bcda2a19c
archie <archiecomik@gmail.com>
2024-03-29 13:41:55 +0000

    feat: add storage

--- monitorNewTokens.ts
+++ monitorNewTokens.ts
@@ -2,7 +2,10 @@ import { rayFee, solanaConnection } from './constants';
 import { storeData } from './utils';
 import fs from 'fs';
 import chalk from 'chalk';
+import path from 'path';
 import { Connection } from '@solana/web3.js';
+
+const dataPath = path.join(__dirname, 'data', 'new_solana_tokens.json');
 
 async function monitorNewTokens(connection: Connection) {
   try {
@@ -14,6 +17,17 @@ async function monitorNewTokens(connection: Connection) {
         }
 
         console.log(chalk.bgGreen(`found new token signature: ${signature}`));
+
+        console.log(chalk.underline(`new token logs: ${logs}`));
+
+        const newTokenData = {
+          lpSignature: signature,
+          timestamp: new Date().toISOString(),
+          logs: logs,
+        };
+
+        //store new tokens
+        await storeData(dataPath, newTokenData);
       },
       'confirmed'
     );
